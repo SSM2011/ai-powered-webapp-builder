04599ce7f690a2eb8b9f60226dfadf67a0c089ec
Subodh <subodhmule2011@gmail.com>
2025-12-17 12:16:54 +0530

    feat: add @inngest/agent-kit dependency and implement hello agent function
    - Added @inngest/agent-kit to package.json dependencies.
    - Updated helloWorld function to use an agent that greets users with enthusiasm.

--- src/app/api/inngest/functions.js
+++ src/app/api/inngest/functions.js
@@ -1,10 +1,24 @@
 import { inngest } from "../../../inngest/client";
+import { openai, createAgent } from "@inngest/agent-kit";
+
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
-  { event: "test/hello.world" },
+  { event: "agent/hello" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello World!` };
+    const helloAgent = createAgent({
+      name:"Hello-agent",
+      description:"A simple agent that say hello",
+      system:"You are a helpful assistant that always greet with enthusiasm",
+      model: openai({model: "gpt-4o-mini"})
+    })
+
+    const {output} = await helloAgent.run("Say Hello to the user!")
+
+    return{
+      message: output[0].content
+    }
+
   },
 );
